61db2dc0463dceb168d1d392a4a15dad8fc10724
Alice Park <67348917+starryyeyed@users.noreply.github.com>
2025-08-29 17:47:57 +0000

    Committed intial draft changes

--- cafe-game/app.py
+++ cafe-game/app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('cafe.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/order', methods=['POST'])
-def handle_order():  # processes new order
-    order_data = request.json
-    print("New order:", order_data)
-
-    # with sqlite3.connect('cafe.db') as conn:
-        # cursor = conn.cursor()
-        # cursor.execute("INSERT INTO orders (item, date) VALUES (?, ?)", (order_data['item'], order_data['time']))
-        # conn.commit()
-    return{'status': 'success'}
+@app.route('/orders')
+def show_orders():
+    conn = get_db_connection()
+    orders = conn.execute('SELECT * FROM orders').fetchall()
+    conn.close()
+    return render_template('orders.html', orders=orders)
 
 if __name__ == '__main__':
     app.run(debug=True)
